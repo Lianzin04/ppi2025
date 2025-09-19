301a75a7dbd07ba37261a272838f48f6933dfb8d
Lianzin04 <xurumelasensata@gmail.com>
2025-09-19 13:31:06 +0000

    seila

---
+++ conta.py
@@ -0,0 +1,8 @@
+# 19/09
+class Conta:
+     def __init__(self, titular, agencia, numero, pix, saldo):
+          self.titular = titular
+          self.agencia = agencia
+          self.numero = numero
+          self.pix = pix
+          self.saldo = saldo
---
+++ main.py
@@ -0,0 +1,5 @@
+from conta import Conta
+
+conta1 = Conta('Kokushibo lua 1', 1, 123, 12345678, 'Koki@gmail.com')
+print(conta1)
+
